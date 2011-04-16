390c7a7831c51672d67e6768beaee56390595939
yasen.terziivanov <yasen.terziivanov@gmail.com>
2011-04-16 13:30:12 +0000

    Added common services.

---
+++ mse10ccenter/src/callcenter/service/administration/IssuesService.java
@@ -0,0 +1,9 @@
+package callcenter.service.administration;
+
+import callcenter.entity.issue.IssueDetail;
+import callcenter.entity.issue.dto.IssueSearchDTO;
+import callcenter.service.base.BaseService;
+
+public interface IssuesService extends BaseService<IssueDetail, IssueSearchDTO> {
+
+}
