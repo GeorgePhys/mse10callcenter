b0c7be87fc7b5014cc863d63b4916a53994908df
stepanbass@gmail.com <stepanbass@gmail.com@f217ee09-5486-299a-30e6-4658c7e0bd89>
2011-04-17 12:35:34 +0000

    registration navigation works!

--- trunk/mse10ccenter/src/callcenter/web/ChangeLanguage.java
+++ trunk/mse10ccenter/src/callcenter/web/ChangeLanguage.java
@@ -9,11 +9,12 @@ import javax.faces.context.FacesContext;
 
 /**
  * @author mse10slb
- *
+ * 
  */
 public class ChangeLanguage {
-	
-	public void changeLocal(String locale){
-		FacesContext.getCurrentInstance().getViewRoot().setLocale(new Locale(locale));
-		}
+
+	public void changeLocal(String locale) {
+		FacesContext.getCurrentInstance().getViewRoot()
+				.setLocale(new Locale(locale));
+	}
 }
---
+++ trunk/mse10ccenter/src/callcenter/web/RegistrationController.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package callcenter.web;
+
+/**
+ * @author mse10slb
+ * 
+ */
+public class RegistrationController {
+	public String register() {
+		System.out.println("maa faaa");
+
+		return "success";
+	}
+}
