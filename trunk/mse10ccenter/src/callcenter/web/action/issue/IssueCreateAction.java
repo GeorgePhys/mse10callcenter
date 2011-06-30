e7e6c038e0b8150741412c5339106a490d2252ea
sgu.siska@gmail.com <sgu.siska@gmail.com@f217ee09-5486-299a-30e6-4658c7e0bd89>
2011-06-30 19:07:47 +0000

    IssueCreateAction

---
+++ trunk/mse10ccenter/src/callcenter/web/action/issue/IssueCreateAction.java
@@ -0,0 +1,25 @@
+package callcenter.web.action.issue;
+
+import java.io.Serializable;
+
+import javax.ejb.EJB;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+import callcenter.entity.issue.IssueDetail;
+import callcenter.service.administration.IssueServiceBean;
+
+@ViewScoped
+@ManagedBean(name = "issueCreateAction")
+public class IssueCreateAction  implements Serializable {
+
+	private static final long serialVersionUID = -5533722715840256982L;
+
+	@EJB
+	private IssueServiceBean issueService;
+
+	public String create(IssueDetail issueDetail) throws Exception {
+		issueService.saveOrUpdate(issueDetail);
+		return null;
+	}
+}
