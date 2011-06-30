84629c1eaefb5c06676c65239ce0fcd12165ff1f
yasen.terziivanov@gmail.com <yasen.terziivanov@gmail.com@f217ee09-5486-299a-30e6-4658c7e0bd89>
2011-06-30 17:43:13 +0000

    Added user logged in filter.

--- trunk/mse10ccenter/src/callcenter/web/action/login/LoginAction.java
+++ trunk/mse10ccenter/src/callcenter/web/action/login/LoginAction.java
@@ -5,12 +5,13 @@ import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpSession;
 
 import callcenter.entity.clients.User;
 import callcenter.service.administration.UserServiceBean;
 
-@ManagedBean(name = "loginAction")
 @SessionScoped
+@ManagedBean(name = "loginAction")
 public class LoginAction {
 
 	@EJB
@@ -28,7 +29,15 @@ public class LoginAction {
 		return "loginSuccessful";
 	}
 
-	public boolean isLoggedin() {
+	public String logout() {
+		setUser(null);
+		HttpSession session = (HttpSession) FacesContext.getCurrentInstance()
+				.getExternalContext().getSession(false);
+		session.invalidate();
+		return "loginPage";
+	}
+
+	public boolean getLoggedIn() {
 		return getUser() != null;
 	}
 
---
+++ trunk/mse10ccenter/src/callcenter/web/filter/IsUserLoggedInFilter.java
@@ -0,0 +1,46 @@
+package callcenter.web.filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebFilter(urlPatterns = "*.jsf")
+public class IsUserLoggedInFilter implements Filter {
+
+	@Override
+	public void destroy() {
+
+	}
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response,
+			FilterChain chain) throws IOException, ServletException {
+		HttpServletRequest servletRequest = (HttpServletRequest) request;
+		HttpServletResponse servletResponse = (HttpServletResponse) response;
+		HttpSession httpSession = servletRequest.getSession(true);
+
+		String requestURL = servletRequest.getRequestURL().toString();
+
+		if (httpSession.getAttribute("user") == null
+				&& !requestURL.contains("login.jsf")) {
+			servletResponse.sendRedirect("/mse10ccenter/pages/login/login.jsf");
+		} else {
+			chain.doFilter(request, response);
+		}
+	}
+
+	@Override
+	public void init(FilterConfig arg0) throws ServletException {
+
+	}
+
+}
