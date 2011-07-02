5781ee12a4a97ad3caede366aa852c84b27b00f1
yasen.terziivanov <yasen.terziivanov@gmail.com>
2011-07-02 12:06:58 +0000

    Added FAQ.

---
+++ mse10ccenter/src/callcenter/service/forum/FaqServiceBean.java
@@ -0,0 +1,30 @@
+package callcenter.service.forum;
+
+import java.util.List;
+
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+
+import callcenter.dto.BaseDTO;
+import callcenter.entity.forum.FAQEntry;
+import callcenter.service.base.BaseServiceBean;
+
+@LocalBean
+@Stateless
+public class FaqServiceBean extends
+		BaseServiceBean<FAQEntry, BaseDTO<FAQEntry>> {
+
+	public static final String QUERY_LOAD_ALL_ENTRIES_KEY = "QUERY_LOAD_ALL_ENTRIES_KEY";
+	public static final String QUERY_LOAD_ALL_ENTRIES = "from FAQEntry";
+
+	@Override
+	public List<FAQEntry> search(BaseDTO<FAQEntry> args, boolean countOnly) {
+		throw new UnsupportedOperationException();
+	}
+
+	@SuppressWarnings("unchecked")
+	public List<FAQEntry> loadAll() {
+		return getEntityManager().createNamedQuery(QUERY_LOAD_ALL_ENTRIES_KEY)
+				.getResultList();
+	}
+}
---
+++ mse10ccenter/src/callcenter/web/action/forum/FAQAction.java
@@ -0,0 +1,61 @@
+package callcenter.web.action.forum;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.ejb.EJB;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+import callcenter.entity.forum.FAQEntry;
+import callcenter.service.forum.FaqServiceBean;
+
+@SessionScoped
+@ManagedBean(name = "faqAction")
+public class FAQAction implements Serializable {
+
+	private List<FAQEntry> entries;
+
+	@EJB
+	private FaqServiceBean serviceBean;
+
+	public String loadAndNavigateToFAQ() {
+		List<FAQEntry> all = serviceBean.loadAll();
+		entries = new ArrayList<FAQEntry>(all);
+		return "faqPage";
+	}
+
+	public void addNew() {
+		entries.add(0, new FAQEntry());
+	}
+
+	public void save() {
+		List<FAQEntry> refreshed = new ArrayList<FAQEntry>();
+		for (FAQEntry faqEntry : getEntries()) {
+			if (faqEntry.getId() == null) {
+				FAQEntry saveOrUpdate = serviceBean.saveOrUpdate(faqEntry);
+				refreshed.add(saveOrUpdate);
+			} else {
+				refreshed.add(faqEntry);
+			}
+		}
+		entries.clear();
+		entries.addAll(refreshed);
+	}
+
+	/**
+	 * @return the entries
+	 */
+	public List<FAQEntry> getEntries() {
+		return entries;
+	}
+
+	/**
+	 * @param entries
+	 *            the entries to set
+	 */
+	public void setEntries(List<FAQEntry> entries) {
+		this.entries = entries;
+	}
+}
