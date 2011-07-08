19fd4bf9ec6459a58fde75fcd36c0cf9342540ea
itsura87@gmail.com <itsura87@gmail.com@f217ee09-5486-299a-30e6-4658c7e0bd89>
2011-07-08 18:10:05 +0000

    Needed SQL you have to insert it once in your test DB. I still have more to add...

---
+++ trunk/mse10ccenter/WebContent/SQL/Holder.sql
@@ -0,0 +1,8 @@
+﻿ -- Complete list of Holders
+INSERT INTO `Holder` (`id`, `lastModifiedDate`, `code`, `descriptionBg`, `descriptionEn`) VALUES 
+(NULL, '2011-07-08 09:08:32', '01', 'Държави', 'Countries'),
+(NULL, '2011-07-08 09:08:32', '02', 'Общини', 'Municipalities'),
+(NULL, '2011-07-08 09:08:32', '03', 'Градове', 'Cities'),
+(NULL, '2011-07-08 09:08:32', '04', 'Привилегии', 'Privileges'),
+(NULL, '2011-07-08 09:08:32', '05', 'Тип на потребителите', 'Type of users'),
+(NULL, '2011-07-08 09:08:32', '06', 'Статус на потребители/фирми', 'Status of users / companies');
