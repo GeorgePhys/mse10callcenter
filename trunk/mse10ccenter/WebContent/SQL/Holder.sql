bc135e05d390c95e897be38ec578e716526f8fcb
itsura87@gmail.com <itsura87@gmail.com@f217ee09-5486-299a-30e6-4658c7e0bd89>
2011-07-12 07:21:20 +0000

    Only Cities Left... but they are really a lot :D

--- trunk/mse10ccenter/WebContent/SQL/Holder.sql
+++ trunk/mse10ccenter/WebContent/SQL/Holder.sql
@@ -1,8 +1,10 @@
-﻿ -- Complete list of Holders
+﻿ -- Always Delete Before Insert Thats THE LAW
+DELETE FROM `Holder` WHERE `Holder`.`code` in (1,2,3,4,5,6);
+ -- Complete list of Holders
 INSERT INTO `Holder` (`id`, `lastModifiedDate`, `code`, `descriptionBg`, `descriptionEn`) VALUES 
-(NULL, '2011-07-08 09:08:32', '01', 'Държави', 'Countries'),
-(NULL, '2011-07-08 09:08:32', '02', 'Общини', 'Municipalities'),
-(NULL, '2011-07-08 09:08:32', '03', 'Градове', 'Cities'),
-(NULL, '2011-07-08 09:08:32', '04', 'Привилегии', 'Privileges'),
-(NULL, '2011-07-08 09:08:32', '05', 'Тип на потребителите', 'Type of users'),
-(NULL, '2011-07-08 09:08:32', '06', 'Статус на потребители/фирми', 'Status of users / companies');
+(NULL, '2011-07-08 09:08:32', '1', 'Държави', 'Countries'),
+(NULL, '2011-07-08 09:08:32', '2', 'Общини', 'Municipalities'),
+(NULL, '2011-07-08 09:08:32', '3', 'Градове', 'Cities'),
+(NULL, '2011-07-08 09:08:32', '4', 'Привилегии', 'Privileges'),
+(NULL, '2011-07-08 09:08:32', '5', 'Тип на потребителите', 'Type of users'),
+(NULL, '2011-07-08 09:08:32', '6', 'Статус на потребители/фирми', 'Status of users / companies');
