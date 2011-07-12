5dbc5a08dbeb1988f8ba245938f5fce097baaba1
itsura87 <itsura87@gmail.com>
2011-07-12 08:27:03 +0000

    Added last updates over nomenclatures now only cities left.

--- mse10ccenter/WebContent/SQL/Holder.sql
+++ mse10ccenter/WebContent/SQL/Holder.sql
@@ -1,5 +1,5 @@
 ﻿ -- Always Delete Before Insert Thats THE LAW
-DELETE FROM `Holder` WHERE `Holder`.`code` in (1,2,3,4,5,6);
+DELETE FROM `Holder` WHERE `Holder`.`code` in (1,2,3,4,5,6,7,8);
  -- Complete list of Holders
 INSERT INTO `Holder` (`id`, `lastModifiedDate`, `code`, `descriptionBg`, `descriptionEn`) VALUES 
 (NULL, '2011-07-08 09:08:32', '1', 'Държави', 'Countries'),
@@ -7,4 +7,6 @@ INSERT INTO `Holder` (`id`, `lastModifiedDate`, `code`, `descriptionBg`, `descri
 (NULL, '2011-07-08 09:08:32', '3', 'Градове', 'Cities'),
 (NULL, '2011-07-08 09:08:32', '4', 'Привилегии', 'Privileges'),
 (NULL, '2011-07-08 09:08:32', '5', 'Тип на потребителите', 'Type of users'),
-(NULL, '2011-07-08 09:08:32', '6', 'Статус на потребители/фирми', 'Status of users / companies');
+(NULL, '2011-07-08 09:08:32', '6', 'Статус на потребители/фирми', 'Status of users / companies'),
+(NULL, '2011-07-08 09:08:32', '7', 'Статуси на поръчка', 'Status of order');
+(NULL, '2011-07-08 09:08:32', '8', 'Тип на продукта', 'Type of product');
