a718a5ec4f0b04c552cceb0d7dc6a7f1aaa9dd0b
sgu.siska@gmail.com <sgu.siska@gmail.com@f217ee09-5486-299a-30e6-4658c7e0bd89>
2011-07-13 18:07:16 +0000

    Update

--- trunk/mse10ccenter/src/callcenter/web/action/issue/IssueCreateAction.java
+++ trunk/mse10ccenter/src/callcenter/web/action/issue/IssueCreateAction.java
@@ -15,30 +15,24 @@ public class IssueCreateAction implements Serializable {
 
 	private static final long serialVersionUID = -5533722715840256982L;
 	private boolean readonly;
-	private boolean disabled;
 	
-
 	private IssueDetail issue;
 
 	@EJB
 	private IssueServiceBean issueService;
 
-	public String create(IssueDetail issueDetail) {
-		issueService.saveOrUpdate(issueDetail);
-		// issue= issueDetail;
+	public String create() {
+		issueService.saveOrUpdate(issue);
 		setReadonly(true);
-		return "successCreateIssue";
+		return null;
 	}
 
 	public void edit(IssueDetail issue) {
 		setReadonly(false);
 
 	}
-	
-	public void editDate(IssueDetail issue) {
-		setDisabled(false);
 
-	}
+	
 
 	// metod 1) nova instaciq na issue(new IssueDetail) i da setna flaga na
 	// false
@@ -50,15 +44,8 @@ public class IssueCreateAction implements Serializable {
 	public void setReadonly(boolean readonly) {
 		this.readonly = readonly;
 	}
+
 	
-	public boolean getDisabled() {
-		return disabled;
-	}
-
-	public void setDisabled(boolean disabled) {
-		this.disabled = disabled;
-	}
-
 	public IssueDetail getIssue() {
 		if (issue == null)
 			issue = new IssueDetail();
