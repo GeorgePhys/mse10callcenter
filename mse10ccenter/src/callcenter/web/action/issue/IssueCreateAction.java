240756eb4da7b33df695b9b537e3315106c0a24a
sgu.siska <sgu.siska@gmail.com>
2011-07-13 19:09:13 +0000

    Update create Issue

--- mse10ccenter/src/callcenter/web/action/issue/IssueCreateAction.java
+++ mse10ccenter/src/callcenter/web/action/issue/IssueCreateAction.java
@@ -15,7 +15,7 @@ public class IssueCreateAction implements Serializable {
 
 	private static final long serialVersionUID = -5533722715840256982L;
 	private boolean readonly;
-	
+
 	private IssueDetail issue;
 
 	@EJB
@@ -32,10 +32,11 @@ public class IssueCreateAction implements Serializable {
 
 	}
 
-	
-
-	// metod 1) nova instaciq na issue(new IssueDetail) i da setna flaga na
-	// false
+	public String createNewIssue() {
+		setReadonly(false);
+		issue = new IssueDetail();
+		return "createNewIssue";
+	}
 
 	public boolean getReadonly() {
 		return readonly;
@@ -45,7 +46,6 @@ public class IssueCreateAction implements Serializable {
 		this.readonly = readonly;
 	}
 
-	
 	public IssueDetail getIssue() {
 		if (issue == null)
 			issue = new IssueDetail();
