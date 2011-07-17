6af5d477f75a1a8fcb5fa18b498b2ce2684c2519
yasen.terziivanov@gmail.com <yasen.terziivanov@gmail.com@f217ee09-5486-299a-30e6-4658c7e0bd89>
2011-07-17 14:45:43 +0000

    changes in user search

--- trunk/mse10ccenter/src/callcenter/web/action/BaseAction.java
+++ trunk/mse10ccenter/src/callcenter/web/action/BaseAction.java
@@ -6,13 +6,48 @@ import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
 
+import callcenter.entity.Entity;
 import callcenter.entity.clients.User;
 
 @SessionScoped
 @ManagedBean(name = "baseAction")
-public class BaseAction implements Serializable {
+public class BaseAction<E extends Entity> implements Serializable {
 
 	private static final long serialVersionUID = 8997153037439218558L;
+
+	private E targetEntity;
+
+	private boolean readonly;
+
+	/**
+	 * @return the readonly
+	 */
+	public boolean isReadonly() {
+		return readonly;
+	}
+
+	/**
+	 * @param readonly
+	 *            the readonly to set
+	 */
+	public void setReadonly(boolean readonly) {
+		this.readonly = readonly;
+	}
+
+	/**
+	 * @return the targetEntity
+	 */
+	public E getTargetEntity() {
+		return targetEntity;
+	}
+
+	/**
+	 * @param targetEntity
+	 *            the targetEntity to set
+	 */
+	public void setTargetEntity(E targetEntity) {
+		this.targetEntity = targetEntity;
+	}
 
 	public boolean getLoggedIn() {
 		return getUser() != null;
--- trunk/mse10ccenter/src/callcenter/web/action/search/UserSearchAction.java
+++ trunk/mse10ccenter/src/callcenter/web/action/search/UserSearchAction.java
@@ -4,11 +4,13 @@ import java.io.Serializable;
 
 import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.SessionScoped;
 
 import callcenter.dto.user.UserSearchDTO;
 import callcenter.entity.clients.User;
 import callcenter.service.administration.UserServiceBean;
+import callcenter.web.action.BaseAction;
 import callcenter.web.action.BaseSearchAction;
 
 @SessionScoped
@@ -18,12 +20,17 @@ public class UserSearchAction extends BaseSearchAction<User, UserSearchDTO>
 
 	private static final long serialVersionUID = -5156711102367948040L;
 
+	@ManagedProperty("#{baseAction}")
+	private BaseAction baseAction;
+
 	@EJB
 	private UserServiceBean service;
 
 	@Override
 	public String previewSearchResult(User result) {
-		return null;
+		getBaseAction().setTargetEntity(result);
+		getBaseAction().setReadonly(true);
+		return "userRegistration";
 	}
 
 	@Override
@@ -36,4 +43,18 @@ public class UserSearchAction extends BaseSearchAction<User, UserSearchDTO>
 		return User.class;
 	}
 
+	/**
+	 * @return the baseAction
+	 */
+	public BaseAction getBaseAction() {
+		return baseAction;
+	}
+
+	/**
+	 * @param baseAction
+	 *            the baseAction to set
+	 */
+	public void setBaseAction(BaseAction baseAction) {
+		this.baseAction = baseAction;
+	}
 }
