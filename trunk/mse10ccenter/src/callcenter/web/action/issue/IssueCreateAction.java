f2fd918576923dfc934378d46086683722f1746e
sgu.siska@gmail.com <sgu.siska@gmail.com@f217ee09-5486-299a-30e6-4658c7e0bd89>
2011-08-06 18:36:41 +0000

    Last Version

--- trunk/mse10ccenter/src/callcenter/web/action/issue/IssueCreateAction.java
+++ trunk/mse10ccenter/src/callcenter/web/action/issue/IssueCreateAction.java
@@ -1,13 +1,17 @@
 package callcenter.web.action.issue;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 
+import callcenter.dto.project.ProjectSearchDTO;
 import callcenter.entity.issue.IssueDetail;
 import callcenter.service.administration.IssueServiceBean;
+import callcenter.service.project.ProjectServiceBean;
 
 @SessionScoped
 @ManagedBean(name = "issueCreateAction")
@@ -17,6 +21,9 @@ public class IssueCreateAction implements Serializable {
 	private boolean readonly;
 
 	private IssueDetail issue;
+	@EJB
+	private ProjectServiceBean projectList;
+	private ProjectSearchDTO dto;
 
 	@EJB
 	private IssueServiceBean issueService;
@@ -31,6 +38,15 @@ public class IssueCreateAction implements Serializable {
 		setReadonly(false);
 
 	}
+	
+	public List<String> getSearchProject() {
+		  List<String> projectNames=new ArrayList<String>();
+		  for(int i=0;0<projectList.search(dto, false).size();i++){
+		   projectNames.add(projectList.search(dto, false).get(i).getName());
+		  }
+		  return projectNames;
+		 }
+	
 
 	public String open(IssueDetail is) {
 		this.issue = is;
@@ -63,5 +79,7 @@ public class IssueCreateAction implements Serializable {
 	public void setIssue(IssueDetail issue) {
 		this.issue = issue;
 	}
+
+	
 
 }
