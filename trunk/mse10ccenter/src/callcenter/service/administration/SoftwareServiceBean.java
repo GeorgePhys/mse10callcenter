b95e6ba9bb3132ded3bdfbc6a5df6873758f3ced
ts.dimitrov8@gmail.com <ts.dimitrov8@gmail.com@f217ee09-5486-299a-30e6-4658c7e0bd89>
2011-08-14 14:46:55 +0000

    eShop - service, bean for software section, status = open

--- trunk/mse10ccenter/src/callcenter/service/administration/SoftwareServiceBean.java
+++ trunk/mse10ccenter/src/callcenter/service/administration/SoftwareServiceBean.java
@@ -1,7 +1,10 @@
 package callcenter.service.administration;
 
+import java.util.List;
+
 import javax.ejb.LocalBean;
 import javax.ejb.Stateless;
+import javax.persistence.Query;
 
 import callcenter.dto.products.SoftwareDTO;
 import callcenter.entity.products.Software;
@@ -10,5 +13,13 @@ import callcenter.service.base.BaseServiceBean;
 @LocalBean
 @Stateless
 public class SoftwareServiceBean extends BaseServiceBean<Software, SoftwareDTO> {
+	public static final String QUERY_LOAD_ALL_SOFTWARE_KEY = "QUERY_LOAD_ALL_SOFTWARE_KEY";
+	public static final String QUERY_LOAD_ALL_SOFTWARE = "from software order by lastModifiedDate desc";
 
+	@SuppressWarnings("unchecked")
+	public List<String> listAllProjectNames() {
+		Query query = getEntityManager().createNamedQuery(
+				QUERY_LOAD_ALL_SOFTWARE_KEY);
+		return query.getResultList();
+	}
 }
