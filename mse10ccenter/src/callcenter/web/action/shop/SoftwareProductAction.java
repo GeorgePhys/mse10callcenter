3a71db292daa605c97d8ea054123f04f1934ca65
ts.dimitrov8 <ts.dimitrov8@gmail.com>
2011-08-14 14:46:55 +0000

    eShop - service, bean for software section, status = open

---
+++ mse10ccenter/src/callcenter/web/action/shop/SoftwareProductAction.java
@@ -0,0 +1,64 @@
+package callcenter.web.action.shop;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.ejb.EJB;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+import callcenter.dto.BaseDTO;
+import callcenter.entity.products.Software;
+import callcenter.service.administration.SoftwareServiceBean;
+import callcenter.service.base.BaseServiceBean;
+import callcenter.web.action.search.datamodel.JPADataModel;
+
+@SessionScoped
+@ManagedBean(name = "softwareProducts")
+@SuppressWarnings("rawtypes")
+public class SoftwareProductAction implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@EJB
+	private SoftwareServiceBean service;
+
+	private List<String> soft;
+
+	private SoftwareDataModel dataModel;
+
+	private static final class SoftwareDataModel extends JPADataModel<Software> {
+
+		private SoftwareDataModel(BaseServiceBean service, BaseDTO dto) {
+			super(service, dto, Software.class);
+		}
+
+		@Override
+		protected Object getId(Software t) {
+			return t.getId();
+		}
+	}
+
+	public List<String> getSoftwareProducts() {
+		soft = service.listAllProjectNames();
+		return soft;
+	}
+
+	public Object getDataModel() {
+		return dataModel;
+	}
+
+	public void setDataModel(SoftwareDataModel dataModel) {
+		this.dataModel = dataModel;
+	}
+
+	public List<String> getSoft() {
+		return soft;
+	}
+
+	public void setSoft(List<String> soft) {
+		this.soft = soft;
+	}
+}
