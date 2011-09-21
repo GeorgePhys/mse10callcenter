b5801b797710d6e7bcfda66ba296190c0c37869a
sgu.siska@gmail.com <sgu.siska@gmail.com@f217ee09-5486-299a-30e6-4658c7e0bd89>
2011-09-21 18:50:39 +0000

    Update Validation Dates in Create Issue

--- trunk/mse10ccenter/src/callcenter/web/validators/NotAfterTodayValidator.java
+++ trunk/mse10ccenter/src/callcenter/web/validators/NotAfterTodayValidator.java
@@ -4,30 +4,34 @@ import java.util.Date;
 
 import javax.faces.application.FacesMessage;
 import javax.faces.component.UIComponent;
+import javax.faces.component.UIInput;
 import javax.faces.context.FacesContext;
 import javax.faces.validator.FacesValidator;
 import javax.faces.validator.Validator;
 import javax.faces.validator.ValidatorException;
 
-@FacesValidator("notAfterToday")
+@FacesValidator("notAfterDateValidator")
 public class NotAfterTodayValidator implements Validator {
 
-	@Override
-	public void validate(FacesContext context, UIComponent component,
-			Object value) throws ValidatorException {
-		Date date = (Date) value;
+    @Override
+    public void validate(FacesContext context, UIComponent component,
+	    Object value) throws ValidatorException {
+	UIInput secondDate = (UIInput) component.getAttributes().get(
+		"compareTo");
 
-		Date date2 = (Date) component.getAttributes().get("compareTo");
-		if (date2 == null) {
-			String message = "Date ended may not be later than today.";
-			throw new ValidatorException(new FacesMessage(message));
-		}
+	Date secondDateValue = (Date) secondDate.getValue();
 
-		if (date.after(date2)) {
-			String message = "Date may not be later than today.";
-			throw new ValidatorException(new FacesMessage(message));
-		}
-
+	if (secondDateValue == null) {
+	    String message = "Date ended may not be later than today.";
+	    throw new ValidatorException(new FacesMessage(message));
 	}
+
+	Date date = (Date) value;
+	if (date.after(secondDateValue)) {
+	    String message = "Date may not be later than today.";
+	    throw new ValidatorException(new FacesMessage(message));
+	}
+
+    }
 
 }
