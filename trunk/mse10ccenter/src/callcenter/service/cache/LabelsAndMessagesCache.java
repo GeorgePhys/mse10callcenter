f7c3a4e85a584e870b93c0203d14a0c8dd83b27e
yasen.terziivanov@gmail.com <yasen.terziivanov@gmail.com@f217ee09-5486-299a-30e6-4658c7e0bd89>
2011-09-23 10:54:00 +0000

    labels and messages cache

---
+++ trunk/mse10ccenter/src/callcenter/service/cache/LabelsAndMessagesCache.java
@@ -0,0 +1,20 @@
+package callcenter.service.cache;
+
+import javax.annotation.PostConstruct;
+import javax.ejb.Singleton;
+import javax.ejb.Startup;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Startup
+@Singleton
+public class LabelsAndMessagesCache {
+
+	@PersistenceContext
+	private EntityManager entityManager;
+
+	@PostConstruct
+	public void init() {
+
+	}
+}
