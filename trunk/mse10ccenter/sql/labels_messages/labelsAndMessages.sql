68738cdb364294675a631e500be0cea0b10b966e
yasen.terziivanov@gmail.com <yasen.terziivanov@gmail.com@f217ee09-5486-299a-30e6-4658c7e0bd89>
2011-09-23 20:02:18 +0000

    labels and messages sql generator

---
+++ trunk/mse10ccenter/sql/labels_messages/labelsAndMessages.sql
@@ -0,0 +1,3 @@
+insert into Holder values(1, '2011-09-23', 1000, 'Съобщения', 'Messages');
+insert into Value values (1, '2011-09-23', 'Грешно потребителско име и/или парола', 'loginFailed', 'Wrong username and/or password', 1000);
+insert into Value values (2, '2011-09-23', 'Грешен защитен код', 'captchaNotMatching', 'CAPTCHA doesn''t match', 1000);
