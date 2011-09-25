4e501be21d0ce6d73458f7daa82bbfac0c42f671
yasen.terziivanov@gmail.com <yasen.terziivanov@gmail.com@f217ee09-5486-299a-30e6-4658c7e0bd89>
2011-09-25 19:58:24 +0000

    deleted login filter (it's functionality will be replaced with permissions + role) password confirmation added on profile

---
+++ trunk/mse10ccenter/src/callcenter/web/validators/EqualPasswordValidator.java
@@ -0,0 +1,35 @@
+package callcenter.web.validators;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.component.UIComponent;
+import javax.faces.component.UIInput;
+import javax.faces.component.html.HtmlInputText;
+import javax.faces.context.FacesContext;
+import javax.faces.validator.FacesValidator;
+import javax.faces.validator.Validator;
+import javax.faces.validator.ValidatorException;
+
+import org.apache.commons.lang.StringUtils;
+
+@FacesValidator("equalPasswordsValidator")
+public class EqualPasswordValidator implements Validator {
+
+    @Override
+    public void validate(FacesContext context, UIComponent component,
+	    Object value) throws ValidatorException {
+	UIInput password = (UIInput) component.getAttributes().get(
+		"passwordFiled");
+
+	String passwordValue = (String) password.getValue();
+	String confirmPassword = (String) value;
+
+	if (!StringUtils.isEmpty(passwordValue)
+		&& !passwordValue.equals(confirmPassword)) {
+	    ((HtmlInputText) component).setSubmittedValue("");
+	    ((HtmlInputText) password).setSubmittedValue("");
+	    String message = "passwords don't match";
+	    throw new ValidatorException(new FacesMessage(
+		    FacesMessage.SEVERITY_ERROR, message, message));
+	}
+    }
+}
