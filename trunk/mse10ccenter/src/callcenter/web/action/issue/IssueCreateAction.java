6c229fc543365622d1263a6a768f25e005ca4911
yasen.terziivanov@gmail.com <yasen.terziivanov@gmail.com@f217ee09-5486-299a-30e6-4658c7e0bd89>
2011-09-26 22:37:36 +0000

    assign project to issue when creating the issue

--- trunk/mse10ccenter/src/callcenter/web/action/issue/IssueCreateAction.java
+++ trunk/mse10ccenter/src/callcenter/web/action/issue/IssueCreateAction.java
@@ -1,15 +1,14 @@
 package callcenter.web.action.issue;
 
 import java.io.Serializable;
-import java.util.List;
 
 import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 
 import callcenter.entity.issue.IssueDetail;
+import callcenter.entity.issue.Project;
 import callcenter.service.administration.IssueServiceBean;
-import callcenter.service.project.ProjectServiceBean;
 import callcenter.util.MailSender;
 import callcenter.web.action.BaseAction;
 
@@ -21,25 +20,21 @@ public class IssueCreateAction extends BaseAction<IssueDetail> implements
     private static final long serialVersionUID = -5533722715840256982L;
 
     @EJB
-    private ProjectServiceBean projectService;
-
-    @EJB
     private IssueServiceBean issueService;
 
-    public String create() {
+    public void create() {
+	IssueDetail issue = issueService.saveOrUpdate(getTargetEntity());
+	setTargetEntity(issue);
 	MailSender.sendCreateIssueMessage(getTargetEntity());
-	issueService.saveOrUpdate(getTargetEntity());
 	setReadonly(true);
-	return null;
+    }
+
+    public void addProject(Project project) {
+	getTargetEntity().setProject(project);
     }
 
     public void edit(IssueDetail issue) {
 	setReadonly(false);
-
-    }
-
-    public List<String> getSearchProject() {
-	return projectService.listAllProjectNames();
     }
 
     public String open(IssueDetail is) {
@@ -50,7 +45,6 @@ public class IssueCreateAction extends BaseAction<IssueDetail> implements
     }
 
     public String createNewIssue() {
-
 	setReadonly(false);
 	setTargetEntity(new IssueDetail());
 	return "createNewIssue";
