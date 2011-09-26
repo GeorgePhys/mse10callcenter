856e9294d59d3beaf60209e974d4f7e454734d5c
sgu.siska <sgu.siska@gmail.com>
2011-09-26 17:17:08 +0000

    Update mail sender assignee

--- mse10ccenter/src/callcenter/util/MailSender.java
+++ mse10ccenter/src/callcenter/util/MailSender.java
@@ -12,6 +12,7 @@ import javax.naming.InitialContext;
 import javax.naming.NamingException;
 
 import callcenter.entity.clients.User;
+import callcenter.entity.issue.IssueDetail;
 
 public final class MailSender {
 
@@ -49,6 +50,36 @@ public final class MailSender {
 	    builder.append("\">http://localhost:8080/mse10ccenter</a><br/><br/>");
 	    builder.append("Please note that you have until " + expirationDate
 		    + " to confirm your registration.<br/>");
+	    message.setContent(builder.toString(), "text/html");
+	    Transport.send(message);
+	} catch (NamingException e) {
+	    e.printStackTrace();
+	} catch (AddressException e) {
+	    e.printStackTrace();
+	} catch (MessagingException e) {
+	    e.printStackTrace();
+	}
+    }
+
+    public static void sendCreateIssueMessage(IssueDetail issueDetail) {
+	try {
+	    javax.mail.Session session = (javax.mail.Session) CommonUtil
+		    .getInitialContext().lookup("java:/Mail");
+	    Message message = new MimeMessage(session);
+
+	    InternetAddress to[] = new InternetAddress[1];
+	    to[0] = new InternetAddress(issueDetail.getAssignee());
+	    message.setRecipients(Message.RecipientType.TO, to);
+
+	    message.setSubject("MSE10 Call Center create issue for you.");
+	    StringBuilder builder = new StringBuilder();
+	    builder.append("Hello " + issueDetail.getAssignee() + ", <br/>");
+	    builder.append("you have recently created issue at MSE10 Call Center.<br/>");
+	    // builder.append("To confirm your registration please click on the following link.<br/>");
+	    // builder.append("<a href=\"http://localhost:8080/mse10ccenter/confirmation?id=");
+	    //
+	    // builder.append("\">http://localhost:8080/mse10ccenter</a><br/><br/>");
+
 	    message.setContent(builder.toString(), "text/html");
 	    Transport.send(message);
 	} catch (NamingException e) {
