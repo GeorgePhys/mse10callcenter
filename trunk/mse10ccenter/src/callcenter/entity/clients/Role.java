b22054dd7158ff7da1a20bfe3a2a0f51b59c63a2
yasen.terziivanov@gmail.com <yasen.terziivanov@gmail.com@f217ee09-5486-299a-30e6-4658c7e0bd89>
2011-09-26 23:43:35 +0000

    use methods in BaseAction to check roles and permissions

--- trunk/mse10ccenter/src/callcenter/entity/clients/Role.java
+++ trunk/mse10ccenter/src/callcenter/entity/clients/Role.java
@@ -4,20 +4,20 @@
 package callcenter.entity.clients;
 
 /**
- * Specifies the type of the user
+ * Specifies the role of the user
  * 
  * @author Stepan Bahdikyan
  */
 public enum Role {
-	USER("User"), ADMIN("Admin"), CLIENT("Client"), EMPLOYEE("Employee");
+    USER("User"), ADMIN("Admin"), CLIENT("Client"), EMPLOYEE("Employee");
 
-	private final String label;
+    private final String label;
 
-	private Role(String label) {
-		this.label = label;
-	}
+    private Role(String label) {
+	this.label = label;
+    }
 
-	public String getLabel() {
-		return this.label;
-	}
+    public String getLabel() {
+	return this.label;
+    }
 }
