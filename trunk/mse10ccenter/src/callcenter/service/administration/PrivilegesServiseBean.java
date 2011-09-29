2c44558b3f208aee409f6867054cb9511648028e
stepanbass@gmail.com <stepanbass@gmail.com@f217ee09-5486-299a-30e6-4658c7e0bd89>
2011-09-29 23:21:31 +0000

    user groups working now!

--- trunk/mse10ccenter/src/callcenter/service/administration/PrivilegesServiseBean.java
+++ trunk/mse10ccenter/src/callcenter/service/administration/PrivilegesServiseBean.java
@@ -5,23 +5,43 @@ package callcenter.service.administration;
 
 import java.util.List;
 
-import javax.ejb.Stateless;
+import javax.ejb.Stateful;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.PersistenceContextType;
 import javax.persistence.Query;
 
-import callcenter.dto.privilege.PrivilegeDTO;
 import callcenter.entity.privileges.Privilege;
-import callcenter.service.base.BaseServiceBean;
 
 /**
  * @author bassextremist
  * 
  */
-@Stateless
-public class PrivilegesServiseBean extends
-	BaseServiceBean<Privilege, PrivilegeDTO> {
+@Stateful
+public class PrivilegesServiseBean {
 
     public static final String QUERY_PRIVILEGES_BY_AREA_KEY = "QUERY_PRIVILEGES_BY_AREA_KEY";
     public static final String QUERY_PRIVILEGES_BY_AREA = "from Privilege p where p.entityArea = :area";
+
+    @PersistenceContext(type = PersistenceContextType.EXTENDED)
+    private EntityManager entityManager;
+
+    // @TransactionAttribute(TransactionAttributeType.REQUIRES_NEW)
+    public <E> E save(E e) {
+	return getEntityManager().merge(e);
+    }
+
+    public <E> E find(Class<E> clazz, Object id) {
+	return getEntityManager().find(clazz, id);
+    }
+
+    public <K> K load(Class<K> clazz, Object id) {
+	return getEntityManager().find(clazz, id);
+    }
+
+    protected EntityManager getEntityManager() {
+	return entityManager;
+    }
 
     public List<Privilege> getPrivilegesByArea(String entityArea) {
 	Query query = getEntityManager().createNamedQuery(
--- trunk/mse10ccenter/src/callcenter/web/action/privileges/CreateGroupAction.java
+++ trunk/mse10ccenter/src/callcenter/web/action/privileges/CreateGroupAction.java
@@ -16,6 +16,7 @@ import callcenter.entity.clients.User;
 import callcenter.entity.privileges.Privilege;
 import callcenter.entity.privileges.PrivilegesGroup;
 import callcenter.service.administration.PrivilegesServiseBean;
+import callcenter.service.administration.UserServiceBean;
 import callcenter.web.action.BaseAction;
 
 /**
@@ -26,12 +27,68 @@ import callcenter.web.action.BaseAction;
 @SessionScoped
 public class CreateGroupAction extends BaseAction<PrivilegesGroup> {
 
+    private List<String> selectedUserArea = new ArrayList<String>();
+    private List<String> selectedForumArea = new ArrayList<String>();
+    private List<String> selectedShopArea = new ArrayList<String>();
+    private List<String> selectedIssueArea = new ArrayList<String>();
+    private List<String> selectedFaqArea = new ArrayList<String>();
+    private List<String> selectedProjectArea = new ArrayList<String>();
+
+    public List<String> getSelectedUserArea() {
+	return selectedUserArea;
+    }
+
+    public void setSelectedUserArea(List<String> selectedUserArea) {
+	this.selectedUserArea = selectedUserArea;
+    }
+
+    public List<String> getSelectedForumArea() {
+	return selectedForumArea;
+    }
+
+    public void setSelectedForumArea(List<String> selectedForumArea) {
+	this.selectedForumArea = selectedForumArea;
+    }
+
+    public List<String> getSelectedShopArea() {
+	return selectedShopArea;
+    }
+
+    public void setSelectedShopArea(List<String> selectedShopArea) {
+	this.selectedShopArea = selectedShopArea;
+    }
+
+    public List<String> getSelectedIssueArea() {
+	return selectedIssueArea;
+    }
+
+    public void setSelectedIssueArea(List<String> selectedIssueArea) {
+	this.selectedIssueArea = selectedIssueArea;
+    }
+
+    public List<String> getSelectedFaqArea() {
+	return selectedFaqArea;
+    }
+
+    public void setSelectedFaqArea(List<String> selectedFaqArea) {
+	this.selectedFaqArea = selectedFaqArea;
+    }
+
+    public List<String> getSelectedProjectArea() {
+	return selectedProjectArea;
+    }
+
+    public void setSelectedProjectArea(List<String> selectedProjectArea) {
+	this.selectedProjectArea = selectedProjectArea;
+    }
+
     @EJB
     private PrivilegesServiseBean privilegesServiseBean;
 
-    private List<User> selectedUsers = new LinkedList<User>();
+    @EJB
+    private UserServiceBean userServiceBean;
 
-    private List<String> selected = new ArrayList<String>();
+    private List<String> selectedUsers = new LinkedList<String>();
 
     public String getGroupName() {
 	return groupName;
@@ -49,21 +106,6 @@ public class CreateGroupAction extends BaseAction<PrivilegesGroup> {
 	List<Privilege> list = privilegesServiseBean
 		.getPrivilegesByArea(entityArea);
 	return list;
-    }
-
-    public List<String> getSelected() {
-	return selected;
-    }
-
-    public void setSelected(List<String> selected) {
-	if (this.selected != null) {
-	    for (String string : selected) {
-		this.selected.add(string);
-	    }
-	} else {
-	    this.selected = selected;
-
-	}
     }
 
     public void setGroupName(String groupName) {
@@ -86,12 +128,56 @@ public class CreateGroupAction extends BaseAction<PrivilegesGroup> {
 
     public String init() {
 	setTargetEntity(new PrivilegesGroup());
+	selectedFaqArea.clear();
+	selectedForumArea.clear();
+	selectedIssueArea.clear();
+	selectedProjectArea.clear();
+	selectedShopArea.clear();
+	selectedUserArea.clear();
+	selectedUsers.clear();
+	groupDescription = null;
+	groupName = null;
 	setReadonly(false);
 	return "createGroup";
     }
 
     public String perview(PrivilegesGroup pg) {
+	init();
+	pg = privilegesServiseBean.find(PrivilegesGroup.class, pg.getId());
 	setTargetEntity(pg);
+
+	this.groupName = pg.getName();
+	this.groupDescription = pg.getDescription();
+
+	for (Privilege privilege : pg.getPrivileges()) {
+	    if ("user".equals(privilege.getEntityArea())) {
+		selectedUserArea.add(privilege.getId().toString());
+		continue;
+	    }
+	    if ("forum".equals(privilege.getEntityArea())) {
+		selectedForumArea.add(privilege.getId().toString());
+		continue;
+	    }
+	    if ("shop".equals(privilege.getEntityArea())) {
+		selectedShopArea.add(privilege.getId().toString());
+		continue;
+	    }
+	    if ("issue".equals(privilege.getEntityArea())) {
+		selectedIssueArea.add(privilege.getId().toString());
+		continue;
+	    }
+	    if ("FAQ".equals(privilege.getEntityArea())) {
+		selectedFaqArea.add(privilege.getId().toString());
+		continue;
+	    }
+	    if ("project".equals(privilege.getEntityArea())) {
+		selectedProjectArea.add(privilege.getId().toString());
+		continue;
+	    }
+	}
+	for (User user : pg.getUsers()) {
+	    selectedUsers.add(user.getId().toString());
+	}
 	setReadonly(true);
 	return "createGroup";
     }
@@ -99,16 +185,41 @@ public class CreateGroupAction extends BaseAction<PrivilegesGroup> {
     public void saveGroup() {
 	setReadonly(true);
 	List<Privilege> realPrevileges = new ArrayList<Privilege>();
-	for (String p : selected) {
+	for (String p : getSelectedFaqArea()) {
+	    realPrevileges.add(privilegesServiseBean.find(Privilege.class,
+		    Long.valueOf(p)));
+	}
+	for (String p : getSelectedForumArea()) {
+	    realPrevileges.add(privilegesServiseBean.find(Privilege.class,
+		    Long.valueOf(p)));
+	}
+	for (String p : getSelectedIssueArea()) {
+	    realPrevileges.add(privilegesServiseBean.find(Privilege.class,
+		    Long.valueOf(p)));
+	}
+	for (String p : getSelectedProjectArea()) {
+	    realPrevileges.add(privilegesServiseBean.find(Privilege.class,
+		    Long.valueOf(p)));
+	}
+	for (String p : getSelectedShopArea()) {
+	    realPrevileges.add(privilegesServiseBean.find(Privilege.class,
+		    Long.valueOf(p)));
+	}
+	for (String p : getSelectedUserArea()) {
 	    realPrevileges.add(privilegesServiseBean.find(Privilege.class,
 		    Long.valueOf(p)));
 	}
 
+	getTargetEntity().getUsers().clear();
+	for (String user : selectedUsers) {
+	    User usr = privilegesServiseBean.load(User.class,
+		    Long.valueOf(user));
+	    usr.getGroups().add(getTargetEntity());
+	    getTargetEntity().getUsers().add(usr);
+	}
+
 	getTargetEntity().getPrivileges().clear();
 	getTargetEntity().getPrivileges().addAll(realPrevileges);
-
-	getTargetEntity().getUsers().clear();
-	getTargetEntity().getUsers().addAll(getSelectedUsers());
 
 	getTargetEntity().setName(groupName);
 	getTargetEntity().setDescription(groupDescription);
@@ -120,7 +231,7 @@ public class CreateGroupAction extends BaseAction<PrivilegesGroup> {
     /**
      * @return the selectedUsers
      */
-    public List<User> getSelectedUsers() {
+    public List<String> getSelectedUsers() {
 	return selectedUsers;
     }
 
@@ -128,8 +239,7 @@ public class CreateGroupAction extends BaseAction<PrivilegesGroup> {
      * @param selectedUsers
      *            the selectedUsers to set
      */
-    public void setSelectedUsers(List<User> selectedUsers) {
-	System.out.println(selectedUsers);
+    public void setSelectedUsers(List<String> selectedUsers) {
 	this.selectedUsers = selectedUsers;
     }
 
